b313e9e55011217df516f21411a56d7db4d3bb7a
Adam Schneider <amschne@umich.edu>
2015-05-27 22:43:10 -0400

    added files to be tracked

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Plot serial data received via the USB port.  Designed for '
+                   'use with Arduino microcontrollers.',
+    'author': 'Adam Schneider',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'amschne@umich.edu',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['plotData'],
+    'scripts': [],
+    'name': 'plotData'
+}
+
+setup(**config)
